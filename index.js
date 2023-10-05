5db5acee668d63d6dbf51cb3d1baa59ef8eba720
Jideotetic <jideotetic@gmail.com>
2023-10-05 06:13:05 +0100

    Implemented the checkAccess function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const result = document.getElementById("get-result");
 const temperature = document.getElementById("convert-temperature");
+const access = document.getElementById("check-access");
 
 function getResult(score) {
   score = prompt("Enter your score");
@@ -11,25 +12,45 @@ function getResult(score) {
 
 result.addEventListener("click", getResult);
 
-function convertTemperature(temp) {
+function convertTemperature(temperature) {
   let s;
-  temp = prompt("Enter temperature in celsius or fahrenheit");
-  s = temp.slice(0, temp.length - 1);
+  temperature = prompt("Enter temperature in celsius or fahrenheit");
+  s = temperature.slice(0, temperature.length - 1);
   while (
-    temp === null ||
-    temp === "" ||
+    temperature === null ||
+    temperature === "" ||
     isNaN(+s) ||
-    (temp.indexOf("F") === -1 &&
-      temp.indexOf("C") === -1 &&
-      temp.indexOf("f") === -1 &&
-      temp.indexOf("c") === -1)
+    (temperature.indexOf("F") === -1 &&
+      temperature.indexOf("C") === -1 &&
+      temperature.indexOf("f") === -1 &&
+      temperature.indexOf("c") === -1)
   ) {
-    temp = prompt("Enter a valid temperature with unit....Hint(10C or 10F)");
-    s = temp.slice(0, temp.length - 1);
+    temperature = prompt(
+      "Enter a valid temperature with unit....Hint(10C or 10F)"
+    );
+    s = temperature.slice(0, temp.length - 1);
   }
-  return temp.slice(-1).toUpperCase() === "C"
-    ? alert(((temp = temp.slice(0, temp.length - 1)) * 9) / 5 + 32 + "F")
-    : alert((temp = temp.slice(0, temp.length - 1) + "F"));
+  return temperature.slice(-1).toUpperCase() === "C"
+    ? alert(
+        ((temperature = temperature.slice(0, temperature.length - 1)) * 9) / 5 +
+          32 +
+          "F"
+      )
+    : alert((temperature = temperature.slice(0, temperature.length - 1) + "F"));
 }
 
 temperature.addEventListener("click", convertTemperature);
+
+function checkAccess(role) {
+  role = prompt("Enter your role (admin | user)");
+  while (role === null || role === "") {
+    role = prompt("Enter a valid role (admin | user)");
+  }
+  return role === "admin"
+    ? alert("Full Access")
+    : role === "user"
+    ? alert("Limited Access")
+    : alert("No Access");
+}
+
+access.addEventListener("click", checkAccess);
