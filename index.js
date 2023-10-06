2705b083ec35f7b175da5b0492929a420f271d87
Jideotetic <jideotetic@gmail.com>
2023-10-06 06:27:15 +0100

    Implemented the menu loop

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const result = document.getElementById("get-result");
 const temperature = document.getElementById("convert-temperature");
 const access = document.getElementById("check-access");
 const evenOdd = document.getElementById("even-odd");
-const bookList = document.getElementById("list-book");
-const libraryDisplay = document.getElementById("library");
-const bookAdd = document.getElementById("add-book");
-const bookRemove = document.getElementById("remove-book");
+const bookMenu = document.getElementById("book-menu");
 
 function getResult(score) {
   score = prompt("Enter your score");
-  while (score === null || score === "" || isNaN(+score)) {
+  while (score === "" || isNaN(+score)) {
     score = prompt("Enter a valid score");
   }
+
+  if (score === null) {
+    return;
+  }
+
   return score >= 60 ? alert("Pass") : alert("Fail");
 }
 
@@ -20,9 +22,12 @@ result.addEventListener("click", getResult);
 function convertTemperature(temperature) {
   let s;
   temperature = prompt("Enter temperature in celsius or fahrenheit");
-  s = temperature.slice(0, temperature.length - 1);
+  if (temperature === null) {
+    return;
+  } else {
+    s = temperature.slice(0, temperature.length - 1);
+  }
   while (
-    temperature === null ||
     temperature === "" ||
     isNaN(+s) ||
     (temperature.indexOf("F") === -1 &&
@@ -33,8 +38,12 @@ function convertTemperature(temperature) {
     temperature = prompt(
       "Enter a valid temperature with unit....Hint(10C or 10F)"
     );
-    s = temperature.slice(0, temp.length - 1);
+    if (temperature === null) {
+      return;
+    }
+    s = temperature.slice(0, temperature.length - 1);
   }
+
   return temperature.slice(-1).toUpperCase() === "C"
     ? alert(
         ((temperature = temperature.slice(0, temperature.length - 1)) * 9) / 5 +
@@ -48,9 +57,14 @@ temperature.addEventListener("click", convertTemperature);
 
 function checkAccess(role) {
   role = prompt("Enter your role (admin | user)");
-  while (role === null || role === "") {
+  while (role === "") {
     role = prompt("Enter a valid role (admin | user)");
   }
+
+  if (role === null) {
+    return;
+  }
+
   return role === "admin"
     ? alert("Full Access")
     : role === "user"
@@ -62,8 +76,12 @@ access.addEventListener("click", checkAccess);
 
 function checkEvenOdd(num) {
   num = prompt("Enter number");
-  while (num === null || num === "" || isNaN(+num)) {
+  while (num === "" || isNaN(+num)) {
     num = prompt("Enter a valid number");
+  }
+
+  if (num === null) {
+    return;
   }
   return num % 2 === 0 ? alert("Even") : alert("Odd");
 }
@@ -78,21 +96,7 @@ function Book(title, author) {
 
 const library = [];
 
-function listBooks() {
-  libraryDisplay.textContent = JSON.stringify(library);
-}
-
-bookList.addEventListener("click", listBooks);
-
 function addBook(title, author) {
-  title = prompt("Enter book title");
-  while (title === null || title === "") {
-    title = prompt("Enter a valid role title");
-  }
-  author = prompt("Enter book author");
-  while (author === null || author === "") {
-    author = prompt("Enter a valid role author");
-  }
   title = title.toLowerCase().split("");
   let titleFirstLetter = title.splice(0, 1).join("").toUpperCase();
   title = title.join("");
@@ -103,27 +107,114 @@ function addBook(title, author) {
   author = authorFirstLetter.concat(author);
   const book = new Book(title, author);
   library.push(book);
-  listBooks();
 }
 
-bookAdd.addEventListener("click", addBook);
+function removeBook(title) {
+  title = title.toLowerCase().split("");
+  let titleFirstLetter = title.splice(0, 1).join("").toUpperCase();
+  title = title.join("");
+  title = titleFirstLetter.concat(title);
+  for (let index = 0; index < library.length; index++) {
+    library[index].title === title
+      ? library.splice(index, 1)
+      : alert("No such book");
+  }
+}
 
-// function removeBook(title) {
-//   title = prompt("Enter book title");
-//   title = title.toLowerCase().split("");
-//   let titleFirstLetter = title.splice(0, 1).join("").toUpperCase();
-//   title = title.join("");
-//   title = titleFirstLetter.concat(title);
-//   for (let index = 0; index < library.length; index++) {
-//     library[index].title === title
-//       ? library.splice(index, 1)
-//       : alert("No such book");
-//   }
-//   libraryDisplay.textContent = JSON.stringify(library);
-// }
+function listBooks() {
+  alert(JSON.stringify(library));
+}
 
-// bookRemove.addEventListener("click", removeBook);
+function markAsRead(title) {
+  title = title.toLowerCase().split("");
+  let titleFirstLetter = title.splice(0, 1).join("").toUpperCase();
+  title = title.join("");
+  title = titleFirstLetter.concat(title);
+  for (let index = 0; index < library.length; index++) {
+    library[index].title === title
+      ? (library[index].isRead = true)
+      : alert("No such book");
+  }
+}
 
-// function markAsRead(title) {
-//   return;
-// }
+function showBookMenu(option) {
+  option = prompt(
+    "Welcome to the Library Management System!\n\n1. Add a book\n2. Remove a book\n3. List all books\n4. Mark a book as read\n5. Exit"
+  );
+
+  if (option === null) {
+    return;
+  }
+
+  while (option === "" || isNaN(+option) || option <= 0 || option > 5) {
+    option = prompt(
+      "Welcome to the Library Management System!\n\nEnter a valid option\n\n1. Add a book\n2. Remove a book\n3. List all books\n4. Mark a book as read\n5. Exit"
+    );
+    if (option === null) {
+      return;
+    }
+  }
+
+  while (option === "1" || option === "2" || option === "3" || option === "4") {
+    if (option === "1") {
+      let title = prompt("Enter book title");
+      while (title === "") {
+        title = prompt("Enter a valid title");
+      }
+      if (title === null) {
+        return;
+      }
+      let author = prompt("Enter book author");
+      while (author === "") {
+        author = prompt("Enter a valid author");
+      }
+      if (author === null) {
+        return;
+      }
+      alert("Book added successfully");
+      addBook(title, author);
+    } else if (option === "2") {
+      let title = prompt("Enter book title");
+      while (title === "") {
+        title = prompt("Enter a valid title");
+      }
+      if (title === null) {
+        return;
+      }
+      alert("Book removed successfully");
+      removeBook(title);
+    } else if (option === "3") {
+      listBooks();
+    } else if (option === "4") {
+      let title = prompt("Enter book title");
+      while (title === "") {
+        title = prompt("Enter a valid title");
+      }
+      if (title === null) {
+        return;
+      }
+      markAsRead(title);
+    }
+
+    option = prompt(
+      "Welcome to the Library Management System!\n\n1. Add a book\n2. Remove a book\n3. List all books\n4. Mark a book as read\n5. Exit"
+    );
+
+    if (option === null) {
+      return;
+    }
+
+    while (option === "" || isNaN(+option) || option <= 0 || option > 5) {
+      option = prompt(
+        "Welcome to the Library Management System!\n\nEnter a valid option\n\n1. Add a book\n2. Remove a book\n3. List all books\n4. Mark a book as read\n5. Exit"
+      );
+      if (option === null) {
+        return;
+      }
+    }
+  }
+
+  alert("Goodbye");
+}
+
+bookMenu.addEventListener("click", showBookMenu);
